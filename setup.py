30c2ddc902bb676b58b1ad06bd8059309d518d5b
Neli Fonseca <neli@ebi.ac.uk>
2021-08-13 00:35:11 +0100

    Pypi test

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='PyProtWorm',
+    version='0.1.0',    
+    description='A python package to find amino acid residues in the literature',
+    url='https://github.com/duenti/PyProtWorm',
+    author='Neli Fonseca',
+    author_email='nelijfjr@gmail.com',
+    license='Apache License 2.0',
+    packages=['PyProtWorm'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: Apache License 2.0',  
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
